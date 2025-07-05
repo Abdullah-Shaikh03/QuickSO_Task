f1e4557bdee7c394a8c5d43f0c99a8178a492a05
Abdullah-Shaikh03 <abdullah.sk0517@gmail.com>
2025-07-06 02:28:25 +0530

    forgot

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,8 +13,19 @@ const app = express()
 const PORT = process.env.PORT || 3001
 
 // Middleware
-app.use(cors())
 app.use(express.json())
+app.use(cors(
+  {
+    origin: [
+      '*',
+      'http://localhost:3000/',
+      'https://quick-so-task.vercel.app/'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }
+))
+// app.options('*', cors())
 app.use(express.urlencoded({ extended: true }))
 
 // Routes
@@ -31,27 +42,39 @@ app.get("/health", (req, res) => {
 // Multer error handler
 app.use(handleMulterError)
 
-// Error handling middleware
-app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
-  console.error(err.stack)
-  res.status(500).json({
-    success: false,
-    error: "Something went wrong!",
-  })
-})
+// // Error handling middleware
+// app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+//   console.error(err.stack)
+//   res.status(500).json({
+//     success: false,
+//     error: "Something went wrong!",
+//   })
+// })
+
+app.get("/debug/env", (req, res) => {
+  res.json({
+    JWT_SECRET: process.env.JWT_SECRET ? "✅ Present" : "❌ Missing",
+    DATABASE_URL: process.env.DATABASE_URL ? "✅ Present" : "❌ Missing",
+    BUCKET_NAME: process.env.BUCKET_NAME ? "✅ Present" : "❌ Missing",
+    NODE_ENV: process.env.NODE_ENV,
+  });
+});
+
 
 // 404 handler
-app.use(
-  (req, res) => {
-    res.status(404).json({
-      success: false,
-      error: "Route not found",
-    })
+// app.use(
+//   (req, res) => {
+//     res.status(404).json({
+//       success: false,
+//       error: "Route not found",
+//     })
+//   }
+// )
+
+if (process.env.NODE_ENV === "dev") {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
   })
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
-
+}
 
 export default app;
